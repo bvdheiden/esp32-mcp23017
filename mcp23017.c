6b4df2220f9cdb88c8994b34c3ba3f487605d3b3
bvdheiden <b.vdh@protonmail.com>
2021-03-20 12:46:03 +0100

    .

--- mcp23017.c
+++ mcp23017.c
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2017 David Antliff
+ * Copyright (c) 2021 Ben van der Heiden
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
